e4c185cabbc766a4024e17589eaacdde20cf68ff
Thitiporn_ji <77443267+faraearn@users.noreply.github.com>
2022-02-07 12:44:42 +0700

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,158 +1,318 @@
+/*Base register adddress header file*/
 #include "stm32l1xx.h"
-#include "stm32l1xx_ll_system.h"
-#include "stm32l1xx_ll_bus.h"
+/*Library related header files*/
 #include "stm32l1xx_ll_gpio.h"
 #include "stm32l1xx_ll_pwr.h"
 #include "stm32l1xx_ll_rcc.h"
+#include "stm32l1xx_ll_bus.h"
 #include "stm32l1xx_ll_utils.h"
-
-/*Timer peripherral driver included*/
+#include "stm32l1xx_ll_system.h"
 #include "stm32l1xx_ll_tim.h"
-
-//LL driver for LCD included
-#include "stm32l1xx_ll_lcd.h"
-#include "stm32l152_glass_lcd.h"
-#include "stdio.h" //for sprintf function access
-
+#include "stm32l1xx_ll_usart.h"
+#include <string.h>
+#include "ESP8266_lowlevel_conf.h"
 
 void SystemClock_Config(void);
-void GPIO_Configuretion (void);
-void TIMBase_Config(void);
-uint32_t cnt = 0;
-uint8_t second = 0;
-uint8_t minute = 0;
-uint8_t hour = 0;
-char display[7];
-
-void Show_segment();
-void Reset_show_segment();
-uint32_t arrtime[4] = {0, 0, 0, 0};
-
-uint8_t i;
-
-uint32_t seg[11] = {LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14,	//0
-										LL_GPIO_PIN_10 | LL_GPIO_PIN_11,	//1
-										LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_15,	//2
-									  LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_15,	//3
-										LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15,	//4
-										LL_GPIO_PIN_2 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15,	//5
-										LL_GPIO_PIN_2 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15,	//6
-										LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11,	//7
-										LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15,	//8
-										LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15,	//9 
-										LL_GPIO_PIN_4}; //L1,L2
+void delay();
 	
-uint32_t digit[4] = {LL_GPIO_PIN_0,LL_GPIO_PIN_1, LL_GPIO_PIN_2, LL_GPIO_PIN_3};
+#define MAX_RESP_BUFFER_SIZE			200
 
-int main()
-{	
-	SystemClock_Config();
-	GPIO_Configuretion();
-	TIMBase_Config();
-
+uint8_t resp[MAX_RESP_BUFFER_SIZE] = {0};
+uint8_t idx;
+char mnum[6];
+char hnum[6];
+/*----------------WIFI config------------------------------------------------------*/
+uint8_t ESP8266_SendCmd(uint8_t* cmd)
+{
+	ESP_USART_LOWLEVEL_Transmit(cmd);
 	while(1)
 	{
- 		cnt = LL_TIM_GetCounter(TIM2);
-		if (cnt >= LL_TIM_GetAutoReload(TIM2))
+		
+		(ESP_USART_LOWLEVEL_Recv(resp, idx) != 1)?(idx = (idx + 1) % MAX_RESP_BUFFER_SIZE):(idx);		
+		if(strstr((const char*)resp, "OK"))
 		{
-			LL_TIM_SetCounter(TIM2, 0);
-			second++;
-			if (second == 60)
-			{
-				second = second%60;
-				minute++;
-				if (minute == 60)
-				{
-					minute = minute%60;
-					hour++;
-				}
-			}
-			arrtime[3] = second%10;
-			arrtime[2] = second/10;
-			arrtime[1] = minute%10;
-			arrtime[0] = minute/10;
+			return 0;
 		}
-		Show_segment(arrtime);
 	}
 }
-
-void Reset_show_segment()
+void ESP_ServerStart()
 {
-	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-	LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15);
-}
-
-void Show_segment(time){
-	for(i=0	; i<4; i++)
+	while(1)
 	{
-		Reset_show_segment();
-		LL_GPIO_SetOutputPin(GPIOB, seg[arrtime[i]]);
-		LL_GPIO_SetOutputPin(GPIOC, digit[i]);
+		
+		(ESP_USART_LOWLEVEL_Recv(resp, idx) != 1)?(idx = (idx + 1) % MAX_RESP_BUFFER_SIZE):(idx);		
+		if(strstr((const char*)resp, "CONNECT"))
+		{
+			return;
+		}
 	}
 }
-
+void ESP8266_RespBufferReset(void)
+{
+	memset(resp, NULL, MAX_RESP_BUFFER_SIZE);
+	idx = 0;
+}
+/*----------------WIFI config------------------------------------------------------*/
 void TIMBase_Config(void)
 {
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+	LL_TIM_EnableCounter(TIM2);
+	
 	/*create object name*/
 	LL_TIM_InitTypeDef timbase;
 	
 	/*enable clock on bus path APB1*/
-	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM9);
 	
 	timbase.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
 	timbase.CounterMode = LL_TIM_COUNTERMODE_UP;
-	timbase.Autoreload = 8000 - 1;
-	timbase.Prescaler = 4000 - 1;
+	//10s
+	timbase.Autoreload = 1000 - 1;
+	timbase.Prescaler = 32000- 1;
 	
-	LL_TIM_Init(TIM2, &timbase);
+	LL_TIM_Init(TIM9, &timbase);
 	
 	/*Enable timer interrupt*/
-	LL_TIM_EnableIT_UPDATE(TIM2);
+	LL_TIM_EnableIT_UPDATE(TIM9);
 	
 	/*Nested vectored interrupt controller*/
-	NVIC_SetPriority(TIM2_IRQn, 0);/*set priority 0, defualt priority 35*/
-	NVIC_EnableIRQ(TIM2_IRQn);/*enable NVIC IRQ channal*/
+	NVIC_SetPriority(TIM9_IRQn, 0);/*set priority 0, defualt priority 35*/
+	NVIC_EnableIRQ(TIM9_IRQn);/*enable NVIC IRQ channal*/
 
-	LL_TIM_EnableCounter(TIM2);
+	LL_TIM_EnableCounter(TIM9);
 }
 
-void GPIO_Configuretion (void)
+void GPIO_Config(void)
 {
-//	LL_GPIO_InitTypeDef gpio_conf;
-//	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-//	gpio_conf.Pin = LL_GPIO_PIN_6;
-//	gpio_conf.Mode = LL_GPIO_MODE_OUTPUT;
-//	gpio_conf.Pull = LL_GPIO_PULL_NO;
-//	gpio_conf.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-//	gpio_conf.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-//	LL_GPIO_Init(GPIOB, &gpio_conf);
-//	
-//	gpio_conf.Pin = LL_GPIO_PIN_7;
-//	LL_GPIO_Init(GPIOB, &gpio_conf);
-//	
-	LL_GPIO_InitTypeDef ltc4727_initstruct;
-	//configure ltc4727js
-	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+		LL_GPIO_InitTypeDef hcsr04_gpio;
+		
+		LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+		
+		hcsr04_gpio.Mode = LL_GPIO_MODE_OUTPUT;
+		hcsr04_gpio.Pull = LL_GPIO_PULL_NO;
+		hcsr04_gpio.Pin = LL_GPIO_PIN_2;
+		hcsr04_gpio.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+		hcsr04_gpio.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+		LL_GPIO_Init(GPIOA, &hcsr04_gpio);
 	
-	ltc4727_initstruct.Mode = LL_GPIO_MODE_OUTPUT;
-	ltc4727_initstruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	ltc4727_initstruct.Pull = LL_GPIO_PULL_NO;
-	ltc4727_initstruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-	ltc4727_initstruct.Pin = LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
-	LL_GPIO_Init(GPIOB, &ltc4727_initstruct);
+		hcsr04_gpio.Mode = LL_GPIO_MODE_INPUT;
+		hcsr04_gpio.Pin = LL_GPIO_PIN_1;
+		hcsr04_gpio.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+		hcsr04_gpio.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+		LL_GPIO_Init(GPIOA, &hcsr04_gpio);
 	
-	ltc4727_initstruct.Pin = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3;
-	LL_GPIO_Init(GPIOC, &ltc4727_initstruct);
-	
+		LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_2);
 }
 
-void TIM2_IRQHandler(void){
-	if(LL_TIM_IsActiveFlag_UPDATE(TIM2) == SET)/*unnecessary because */
-	{
-		LL_TIM_ClearFlag_UPDATE(TIM2);
-		//Once update event occure, program jump to here
-	}
+
+void GPIO7sec_Config(void)
+{
+		LL_GPIO_InitTypeDef ltc4727_initstruct;
+	  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+	  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+	  ltc4727_initstruct.Mode = LL_GPIO_MODE_OUTPUT;
+		ltc4727_initstruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+		ltc4727_initstruct.Pull = LL_GPIO_PULL_NO;
+		ltc4727_initstruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+		ltc4727_initstruct.Pin = LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 |  LL_GPIO_PIN_15;
+		LL_GPIO_Init(GPIOB, &ltc4727_initstruct);
+	
+		ltc4727_initstruct.Pin = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3;
+		LL_GPIO_Init(GPIOC, &ltc4727_initstruct);
+	
+}
+uint16_t rise_timestamp = 0;
+uint16_t fall_timestamp = 0;
+uint16_t up_cycle = 0;
+
+uint8_t state = 0;
+float period = 0;
+float distance = 0;
+int s = 0;
+uint32_t TIM2CLK;
+uint32_t PSC;
+uint32_t cnt = 0;
+uint8_t second = 0;
+uint8_t minute = 0;
+uint8_t hour = 0;
+uint8_t h1 = 0;
+uint8_t h2 = 0;
+uint8_t m1 =0 ;
+uint8_t m2 = 0 ;
+int main()
+{    
+		SystemClock_Config();
+	  uint8_t  o;
+	  uint8_t check_digit = 4;
+	  uint32_t number[10] = {LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 , 							
+												 LL_GPIO_PIN_13 | LL_GPIO_PIN_14 , 																																									
+												 LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_15 | LL_GPIO_PIN_13 | LL_GPIO_PIN_12, 														
+												 LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_15 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12	,																
+												 LL_GPIO_PIN_15 | LL_GPIO_PIN_14 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11,																							
+												 LL_GPIO_PIN_2 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12,																
+												 LL_GPIO_PIN_2 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13,								
+												 LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 ,																															
+												 LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15, 
+											 	 LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15						
+												};
+		uint32_t digit[4] = {LL_GPIO_PIN_0 , LL_GPIO_PIN_1, LL_GPIO_PIN_2,LL_GPIO_PIN_3};
+		GPIO_Config();
+		GPIO7sec_Config();
+		TIMBase_Config();
+		
+	ESP8266_SendCmd((uint8_t*)"AT+RST\r\n");
+	ESP8266_RespBufferReset();	
+	ESP8266_SendCmd((uint8_t*)"AT+RESTORE\r\n");
+	ESP8266_RespBufferReset();
+	LL_mDelay(1000); //Prevent ESP8266 flooding message
+	ESP8266_SendCmd((uint8_t*)"AT+CWMODE=1\r\n");
+	ESP8266_RespBufferReset();
+	ESP8266_SendCmd((uint8_t*)"AT+RST\r\n");
+	ESP8266_RespBufferReset();	
+	LL_mDelay(1000);
+	ESP8266_SendCmd((uint8_t*)"AT+CWJAP=\"padthai_2G\",\"1911310231\"\r\n");
+	ESP8266_RespBufferReset();	
+	ESP8266_SendCmd((uint8_t*)"AT+CWJAP?\r\n");
+	ESP8266_RespBufferReset();	
+	LL_mDelay(1000);
+    
+		while(1)
+		{
+			if (s ==0)
+			{
+			switch(state)
+			{
+				case 0:
+					//Trigger measurement
+					LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_2);
+					LL_mDelay(1);
+					LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_2);
+					state = 1;
+				break;
+				
+				case 1:
+					if(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_1))
+					{
+						rise_timestamp = LL_TIM_GetCounter(TIM2);
+						state = 2;
+					}
+				break;
+					
+				case 2:
+					if(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_1) == RESET)
+					{
+						fall_timestamp = LL_TIM_GetCounter(TIM2);
+						//Calculate uptime
+						if(fall_timestamp > rise_timestamp)
+						{
+							up_cycle = fall_timestamp - rise_timestamp;
+						}
+						else if(fall_timestamp < rise_timestamp)
+						{
+							up_cycle = (LL_TIM_GetAutoReload(TIM2) - rise_timestamp) + fall_timestamp + 1; 
+						}
+						else
+						{
+							//cannot measure at this freq
+							up_cycle = 0;
+						}
+						
+						if(up_cycle != 0)
+						{
+							PSC = LL_TIM_GetPrescaler(TIM2) + 1;
+							TIM2CLK = SystemCoreClock / PSC;
+							
+							period = (up_cycle*(PSC) * 1.0) / (TIM2CLK * 1.0); //calculate uptime period
+							distance = (period * 340) / 2; //meter unit
+							if(distance<= 0.05)
+							{
+							cnt = LL_TIM_GetCounter(TIM9);
+									//if (cnt >= LL_TIM_GetAutoReload(TIM9))
+									if (cnt >= 5)
+									{
+										LL_TIM_SetCounter(TIM9, 0);
+										second++;
+										if (second == 60)
+										{
+											second = second%60;
+											minute++;
+											if (minute == 60)
+											{
+												minute = minute%60;
+												hour++;
+											}
+										}
+										  break;
+									}
+							}
+							else
+							{
+								if (second > 10)
+									s = 1;
+								else 
+									s = 0;
+							}
+						}
+						state = 0;
+					}
+				break;
+			}
+		}
+			else
+				break;
+
+   }
+	SystemClock_Config();
+	ESP_USART_LOWLEVEL_Conf();
+	ESP_USART_Start();
+	
+
+	 /*----------------CLIENT------------------------------------------------------*/
+	sprintf(hnum, "%d", hour);
+	sprintf(mnum, ":%d", minute);
+	strcat(hnum,mnum);
+	ESP8266_SendCmd((uint8_t*)"AT+CIPMUX=0\r\n");
+  ESP8266_RespBufferReset();
+  ESP8266_SendCmd((uint8_t*)"AT+CIPSTART=\"TCP\",\"192.168.1.42\",1102\r\n");
+  ESP8266_RespBufferReset();
+  	
+  ESP8266_SendCmd((uint8_t*)"AT+CIPSEND=5\r\n");
+  ESP8266_RespBufferReset();
+	ESP_USART_LOWLEVEL_Transmit((uint8_t *)hnum);
+  m1 = minute/10;
+	m2 = minute%10;
+	h1 = hour/10;
+	h2 = hour%10;
+	 
+	 while(1){
+		
+		for (o = 0 ; o < 5 ; o++){
+			LL_GPIO_ResetOutputPin(GPIOC,LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+			LL_GPIO_ResetOutputPin(GPIOB,LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 |
+															LL_GPIO_PIN_13 | LL_GPIO_PIN_14 |  LL_GPIO_PIN_15);
+		switch (o){
+			case 0:
+				LL_GPIO_SetOutputPin(GPIOB,number[h1]);
+				LL_GPIO_SetOutputPin(GPIOC,digit[o]);
+				break;
+			case 1:
+				LL_GPIO_SetOutputPin(GPIOB,number[h2]);
+				LL_GPIO_SetOutputPin(GPIOC,digit[o]);
+			case 2:
+				LL_GPIO_SetOutputPin(GPIOB,number[m1]);
+				LL_GPIO_SetOutputPin(GPIOC,digit[o]);
+				break;
+			case 3:
+				LL_GPIO_SetOutputPin(GPIOB,number[m2]);
+				LL_GPIO_SetOutputPin(GPIOC,digit[o]);
+				break;
+			case 4:
+				LL_GPIO_SetOutputPin(GPIOB,LL_GPIO_PIN_2|LL_GPIO_PIN_10);
+			}
+		//LL_mDelay(1000);
+		
+		}
+  }
 }
 
 void SystemClock_Config(void)
@@ -207,4 +367,16 @@ void SystemClock_Config(void)
   
   /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
   LL_SetSystemCoreClock(32000000);
+}
+void TIM9_IRQHandler(void){
+	if(LL_TIM_IsActiveFlag_UPDATE(TIM9) == SET)/*unnecessary because */
+	{
+		LL_TIM_ClearFlag_UPDATE(TIM9);
+		//Once update event occure, program jump to here
+	}
+}
+void delay()
+{
+	unsigned int i = 0;
+	for (i=0; i<100; ++i);
 }
